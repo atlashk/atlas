6b0776d44042c5d2368de8c6cacc6552d44f19a5
Nguyen Tuan Anh <anhnt@lgcns.com>
2025-06-19 17:39:24 +0900

    xxx

---
+++ backend/infrastructure/spring-boot/observability/tracing/tracing-zipkin/src/main/java/org/atlas/infrastructure/observability/tracing/zipkin/TracingConfig.java
@@ -0,0 +1,14 @@
+package org.atlas.infrastructure.observability.tracing.zipkin;
+
+import io.micrometer.context.ContextSnapshotFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TracingConfig {
+
+  @Bean
+  public ContextSnapshotFactory contextSnapshotFactory() {
+    return ContextSnapshotFactory.builder().build();
+  }
+}
