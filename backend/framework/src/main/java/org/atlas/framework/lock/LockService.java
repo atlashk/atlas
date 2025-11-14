be02933c952931813a8b22b940137da1e98a9258
andy <>
2025-11-14 22:33:08 +0700

    fix build

--- backend/framework/src/main/java/org/atlas/framework/lock/LockService.java
+++ backend/framework/src/main/java/org/atlas/framework/lock/LockService.java
@@ -7,9 +7,11 @@ import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.atlas.framework.kvstore.KvStoreService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnBean(KvStoreService.class)
 @RequiredArgsConstructor
 @Slf4j
 public class LockService {
