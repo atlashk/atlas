9c902ab4112d270005385b5b57aa735b1ff0a8ea
andy <andy@atlas.org>
2026-03-03 18:26:00 +0900

    fix kc

--- backend/services/identity-service/identity-infrastructure/identity-migration-flyway-mysql/src/main/resources/db/migration/V001__init_schema.sql
+++ backend/services/identity-service/identity-infrastructure/identity-migration-flyway-mysql/src/main/resources/db/migration/V001__init_schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS users
     first_name   VARCHAR(255) NOT NULL,
     last_name    VARCHAR(255) NOT NULL,
     email        VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
     role         VARCHAR(50)  NOT NULL,
     created_at   DATETIME     NOT NULL,
     updated_at   DATETIME,
--- backend/services/identity-service/identity-infrastructure/identity-migration-flyway-postgres/src/main/resources/db/migration/V001__init_schema.sql
+++ backend/services/identity-service/identity-infrastructure/identity-migration-flyway-postgres/src/main/resources/db/migration/V001__init_schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS users
     first_name   VARCHAR(255) NOT NULL,
     last_name    VARCHAR(255) NOT NULL,
     email        VARCHAR(255) NOT NULL UNIQUE,
-    phone_number VARCHAR(255) NOT NULL UNIQUE,
+    phone_number VARCHAR(20) NOT NULL UNIQUE,
     role         VARCHAR(50)  NOT NULL,
     created_at   TIMESTAMP    NOT NULL,
     updated_at   TIMESTAMP
